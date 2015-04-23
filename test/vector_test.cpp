ab8ed9d6483dc67869147cd30ee911ebcc1b696b
qmick <qmick@live.cn>
2015-04-23 20:12:39 +0800

    Add unit test

---
+++ test/vector_test.cpp
@@ -0,0 +1,23 @@
+#include "../vector.hpp"
+#include <cassert>
+
+int main()
+{
+	using namespace nano;
+	vector<vector<int> > v;
+	vector<int> v1;
+
+	
+	for(int i = 0; i < 1000; ++i)
+	{
+		v1.push_back(i);
+	}
+
+	v.push_back(v1);
+	
+	for (size_t i = 0; i < v[0].size(); ++i)
+	{
+		assert(v[0][i] == i);
+	}
+	return 0;
+}
